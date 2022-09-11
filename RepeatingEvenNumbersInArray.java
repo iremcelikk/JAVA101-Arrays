2a84482d30629bdd2be1d39dd4cac5cc10e179a5
iremcelikk <102186287+iremcelikk@users.noreply.github.com>
2022-09-11 17:20:04 +0300

    RepeatingEvenNumbersInArray
    Finds repeating even numbers in a given array and finds out how many times it is repeated.

---
+++ RepeatingEvenNumbersInArray.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class RepeatingEvenNumbersInArray {
+
+	static void findRepeatingNumbers(int[] array) {
+		String[] strArray = new String[array.length];
+
+		for (int i = 0; i < array.length; i++) {
+			strArray[i] = String.valueOf(array[i]);
+		}
+
+		for (int i = 0; i < strArray.length - 1; i++) {
+			int counter = 1;
+			for (int j = i + 1; j < strArray.length; j++) {
+				if (strArray[i].equals(strArray[j])) {
+					counter++;
+					strArray[j] = "Already counted";
+				}
+			}
+
+			if (counter > 1 && !(strArray[i].equals("Already counted")) && array[i] % 2 == 0) {
+				System.out.println("Even number " + array[i] + " is repeting " + counter + " times.");
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the length of array");
+		int arrayLength = sc.nextInt();
+		int[] array = new int[arrayLength];
+
+		for (int i = 0; i < arrayLength; i++) {
+			System.out.println("Enter the " + (i + 1) + ". element of the array:");
+			array[i] = sc.nextInt();
+		}
+
+		System.out.println(Arrays.toString(array));
+		findRepeatingNumbers(array);
+	}
+
+}
