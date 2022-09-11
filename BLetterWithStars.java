692db232f74d84b9d9b822b4fd8e57842a2c6fca
iremcelikk <102186287+iremcelikk@users.noreply.github.com>
2022-09-11 17:28:50 +0300

    Update BLetterWithStars.java

--- BLetterWithStars.java
+++ BLetterWithStars.java
@@ -2,10 +2,10 @@
 public class BLetterWithStars {
 
 	public static void main(String[] args) {
-		int[][] list = new int[8][4];
+		int[][] array = new int[8][4];
 
-		for (int i = 0; i < list.length; i++) {
-			for (int j = 0; j < list[i].length; j++) {
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
 				if ((j == 0) || (j == 3 && (i != 0) && (i != 3) && (i != 7))
 						|| ((i == 0 || i == 3 || i == 7) && (j != 3))) {
 					System.out.print(" * ");
